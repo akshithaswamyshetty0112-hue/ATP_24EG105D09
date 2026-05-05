4d6119f0481d48029557688e2fe632a611407d00
akshithaswamyshetty0112-hue <akshithaswamyshetty0112@gmail.com>
2026-05-05 15:36:40 +0530

    Update searchUsingFunctions.js

--- week1/searchUsingFunctions.js
+++ week1/searchUsingFunctions.js
@@ -1,3 +1,4 @@
+//function to search in a array
 function search(marks,b){
     for(let index=0;index<marks.length;index++){
         if(b==marks[index]){
@@ -8,6 +9,9 @@ function search(marks,b){
         }
     }
 }
+//initialization of array
 marks=[8,9,7,3]
+//passing array and element to search as parameters and storing the result in a variable
 let result=search(marks,8)
+//printing the result
 console.log(result)

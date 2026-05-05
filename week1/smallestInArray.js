d60840a9cef4b9afa916e60c8eceb7df3cb10bfc
akshithaswamyshetty0112-hue <akshithaswamyshetty0112@gmail.com>
2026-05-05 15:37:58 +0530

    Update smallestInArray.js

--- week1/smallestInArray.js
+++ week1/smallestInArray.js
@@ -1,8 +1,11 @@
+//initialization of array
 let marks=[90,70,80,60,50]
+//logic for finding smallest in a array
 let a=marks[0]
 for(let index=0;index<marks.length;index++){
     if(a>marks[index]){
         a=marks[index]
     }
 }
+//printing the result
 console.log("smallest number is",a)
